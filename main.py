56798f9fa0dce1505c99913045fb7c4465a82fa4
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-03-01 17:19:42 +0600

    Feat: add and show cases updated with 'with...as' context manager

--- main.py
+++ main.py
@@ -7,19 +7,21 @@ while True:
         case "add":
             todo = input("Enter a todo: ").capitalize() + "\n"
 
-            file = open("files/todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+            with open("files/todos.txt", 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open("files/todos.txt", "w")
-            file.writelines(todos)
-            file.close()
+            with open("files/todos.txt", 'w') as file:
+                file.writelines(todos)
+
         case "show" | "display":
             file = open("files/todos.txt", "r")
             todos = file.readlines()
             file.close()
+
+            with open("files/todos.txt", "r") as file:
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
                 items = (f"{index + 1}-{item}").strip("\n")
