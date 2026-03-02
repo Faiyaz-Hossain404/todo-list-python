3acce80c54f256b520dc07d34003fda2253190ef
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-03-02 15:14:05 +0600

    Fix: match case removed and if statements added

--- main.py
+++ main.py
@@ -3,59 +3,58 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case "add":
-            todo = input("Enter a todo: ").capitalize() + "\n"
+    if "add" in user_action:
+        todo = user_action[4:] + "\n"
 
-            with open("files/todos.txt", 'r') as file:
-                todos = file.readlines()
-
-            todos.append(todo)
-
-            with open("files/todos.txt", 'w') as file:
-                file.writelines(todos)
-
-        case "show" | "display":
-            file = open("files/todos.txt", "r")
+        with open("files/todos.txt", 'r') as file:
             todos = file.readlines()
-            file.close()
 
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
+        todos.append(todo)
 
-            for index, item in enumerate(todos):
-                items = (f"{index + 1}-{item}").strip("\n")
-                print(items)
-        case "edit":
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
+        with open("files/todos.txt", 'w') as file:
+            file.writelines(todos)
 
-            with open("files/todos.txt", 'r') as file:
-                todos = file.readlines()
+    if "show" in user_action:
+        file = open("files/todos.txt", "r")
+        todos = file.readlines()
+        file.close()
 
-            new_todo = input("Enter new todo: ").capitalize()
-            todos[number] = new_todo
+        with open("files/todos.txt", "r") as file:
+            todos = file.readlines()
 
-            with open("files/todos.txt", 'w') as file:
-                file.writelines(todos)
-        case "complete":
-            number = int(input("Number of the todo to complete: "))
+        for index, item in enumerate(todos):
+            items = (f"{index + 1}-{item}").strip("\n")
+            print(items)
+    if "edit" in user_action:
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
 
-            with open("files/todos.txt", 'r') as file:
-                todos = file.readlines()
-            
-            index = number - 1
+        with open("files/todos.txt", 'r') as file:
+            todos = file.readlines()
 
-            removed_removed = todos[index].strip()
+        new_todo = input("Enter new todo: ").capitalize()
+        todos[number] = new_todo
 
-            todos.pop(index)
+        with open("files/todos.txt", 'w') as file:
+            file.writelines(todos)
+    if "complete" in user_action:
+        number = int(input("Number of the todo to complete: "))
 
-            with open("files/todos.txt", 'w') as file:
-                file.writelines(todos)       
+        with open("files/todos.txt", 'r') as file:
+            todos = file.readlines()
+        
+        index = number - 1
 
-            message = f"Todo {removed_removed} was removed from the list"
-            print(message)  
-        case "exit":
-            break
+        removed_removed = todos[index].strip()
+
+        todos.pop(index)
+
+        with open("files/todos.txt", 'w') as file:
+            file.writelines(todos)       
+
+        message = f"Todo {removed_removed} was removed from the list"
+        print(message)  
+    if "exit" in user_action:
+        break
 
 print("Bye!");
