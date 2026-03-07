7ed2e9b91c65a43f88a8fe6dfc2f7aa7802a1bfe
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-03-07 16:20:10 +0600

    Feat: functions separated into different file

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def get_todos(filepath = "files/todos.txt"):
-    with open(filepath, "r") as file:
-        todos = file.readlines()
-        return todos
-
-
-def write_todos(todos_arg, filepath = "files/todos.txt"):
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
+from functions import get_todos, write_todos
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -45,6 +36,7 @@ while True:
             print("Your command is not valid. Enter a number.")
 
             continue
+ 
 
     elif user_action.startswith("complete"):
         try:
