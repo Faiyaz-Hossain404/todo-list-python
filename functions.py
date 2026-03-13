26c64f98fb26810ba72bff81052e8b4a04df49b6
Faiyaz Hossain <sifathhossain115@gmail.com>
2026-03-14 01:42:42 +0600

    Feat: windows standalone executable created

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
-def get_todos(filepath = "files/todos.txt"):
+def get_todos(filepath = "todos.txt"):
     """Read a rext file and return list of to-do items"""
     with open(filepath, "r") as file:
         todos = file.readlines()
         return todos
 
 
-def write_todos(todos_arg, filepath = "files/todos.txt"):
+def write_todos(todos_arg, filepath = "todos.txt"):
     """Write the todo items list in the file"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
